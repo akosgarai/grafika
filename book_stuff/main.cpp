9d5337b64350549decb48daa8fce287a29d3c561
akosgarai <szlem01@gmail.com>
2013-06-15 15:59:57 +0200

    C practice + larce book init

---
+++ book_stuff/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//const variables
+const int bagcapacity = 10;
+
+//Stuffs, has amount, name. the character can collet and use them.
+class Stuff {
+private:
+    int stuffAmount;        //amount
+    string stuffName;       //name
+public:
+    Stuff() { this->stuffAmount = 0; this->stuffName = '.'; }                       //default constructor
+    Stuff(string name) { this->stuffAmount = 0; this->stuffName = name; }           //name constructor,
+    Stuff(int num, string name) { this->stuffAmount = num; this->stuffName = name; }    //name and amount
+
+    //get methods
+    int getAmount() { return this->stuffAmount; }
+    string getName() { return this->stuffName; }
+
+    //add method increase the amount
+    void add(int i) {
+        this->stuffAmount +=i;
+        cout << "You found " << i << " new " << this->getName() <<endl;
+    }
+
+    //use method, decrease the amount if possible
+    void use(int i) {
+        if(stuffAmount > i) {
+            this->stuffAmount -= i;
+            cout << "You lost " << i << " piece of " << this->getName() <<endl;
+        }
+        else { cout << "You have less than " << i <<" " << this->getName() <<" You have only " << this->getAmount() << endl; }
+    }
+};
+
+//bag to store bagcapacity number of stuffs
+class Bag {
+private:
+    Stuff myStuffs[bagcapacity];                    //how many different kind of stuffs could be stored
+public:
+    int stuffNum;                                   //the number of stuffs
+    Bag() { stuffNum = 0; }                         //default constructor, in the beginning we have nothing
+
+    //put a new stuff to the bag or rase the amount if the stuff isn't new
+    void addStuff(Stuff s) {
+        if(stuffNum < bagcapacity) {
+            for(int i = 0; i < stuffNum; i++) {
+                if(this->myStuffs[i].getName() == s.getName()) {
+                    this->myStuffs[i].add(s.getAmount());
+                    return;
+                }
+            }
+            myStuffs[stuffNum] = s;
+            stuffNum++;
+            cout << "Your found your " << stuffNum <<". stuff: " << this->myStuffs[stuffNum-1].getName() << "\n";
+        } else {
+            cout << "Sorry, but you are full. You should drop something.\n";
+        }
+    }
+
+    //returns true if there is a stuff in the bag which named s and has greater amount than 0
+    bool getStuff(string s) {
+        for(int i = 0; i < stuffNum; i++) {
+            if(this->myStuffs[i].getName() == s && this->myStuffs[i].getAmount() > 0) {
+                    return true;
+            }
+            return false;
+        }
+    }
+};
+
+//Stuffs like ammo for weapons, medikit, and other fun
+Stuff normalGun(8, "Pisztoly");
+Stuff shotGun("Shotgun");
+Stuff machineGun("Geppuska");
+
+//the player class
+class Character {
+private:
+    int life;           //life points
+    int IP;             //IP-s for doing things
+    Bag myBag;          //collection of stuffs that player can use
+public:
+    Character() { this->life = 10; this->IP = 0; this->myBag.addStuff(normalGun); this->myBag.addStuff(shotGun); this->myBag.addStuff(machineGun); }
+    int getLife() { return this->life; }        //returns the current lifepoints
+    void writeLife() { cout << "Lara\'s life: " << this->life << endl;}     //prints the current lifepoints
+    void writeIP() { cout << "Lara\'s IP number: " << this->IP << endl; }   //prints the current IP points
+    void modifyLife(int i) { this->life -= i;                               //modify lifepoints (heal or injury)
+        if (this->life <= 0)
+            cout << "You died.\n";
+        if (this->life > 10) {
+            this->life = 10;
+            cout << "Your life is full, you can\'t be healthier!\n";
+        }
+    }
+    int getIP() { return this->IP; }
+    void setIP(int i) { this->IP += i; }
+    bool findInBag(string s) {
+        return this->myBag.getStuff(s);
+    }
+
+};
+//And the player is:
+Character Lara;
+
+class Chapter {
+private:
+    int chapterNumber;
+    int nextChapters[5];
+public:
+
+};
+int main()
+{
+
+    Lara.writeLife();
+    Lara.modifyLife(13);
+    Lara.writeLife();
+    Lara.modifyLife(-103);
+    Lara.writeLife();
+    Lara.writeIP();
+    return 0;
+}
---
+++ cPractice/main.c
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+/*
+//3-al oszthato szamok 100-200 kozott
+    int szam = 100;
+    while (szam <= 200) {
+        if (szam % 3 == 0)
+            printf("%d, ", szam);
+        szam++;
+    }
+*/
+/*
+//szam primtenyezos felbontasa
+    int szam = 91;
+    int oszto = 1;
+    while ( szam + 1 != oszto) {
+        if (oszto > 1 && szam % oszto == 0) {
+            printf("%d|%d\n", szam, oszto);
+            szam = szam / oszto;
+            oszto = 1;
+        } else {
+            //printf("nemoszto: %d\n", oszto);
+            oszto++;
+        }
+    }
+    printf("1|1\n");
+*/
+//szam primtenyezoinek osszege
+int primtenyezokosszege(int input) {
+    int oszto, sum;
+    oszto = 1;
+    sum = 0;
+    while (input + 1 != oszto) {
+        if (oszto > 1 && input % oszto == 0) {
+            sum += oszto;
+            input = input / oszto;
+            oszto = 2;
+        } else {
+            oszto++;
+        }
+    }
+    return sum;
+}
+/*
+//valodi osztok szama + prim szam-e
+    int szam = 7;
+    int osztokszama = 0;
+    int oszto = 2;
+    while ( oszto < szam ) {
+        if (szam % oszto == 0)
+            osztokszama++;
+        oszto++;
+    }
+    if (osztokszama == 0) { printf("A szam prim!\n"); }
+    else { printf("Valodi osztok szama: %d\n", osztokszama); }
+*/
+/*
+//legnagyobb kozos osztot kiszamolo fuggveny
+int lnko(int s1, int s2){
+    int kisebb;
+    int oszto = 1;
+    int lnko = 1;
+    if (s1 < s2) kisebb = s1;
+    else kisebb = s2;
+    while ( kisebb + 1 > oszto) {
+        if (s1 % oszto == 0 && s2 % oszto == 0) lnko = oszto;
+        oszto++;
+    }
+    return lnko;
+}
+    int a = lnko(150, 60);
+    printf("lnko: %d\n", a);
+*/
+/*
+//masodfoku egyenlet megoldasa
+int masodfoku(double a, double b, double c) {
+    double disc, x1, x2;
+    disc = b * b + 4 * a * c;
+    if (disc > 0) {
+        x1 = (-b + sqrt(disc)) / (2 * a);
+        x2 = (-b - sqrt(disc)) / (2 * a);
+        printf("A gyokok:\n\tx1 = %g\tx2 = %g\n", x1, x2);
+    } else printf("Nincs valos gyok.\n");
+    return 0;
+}
+int valtozoBeolvasas() {
+    double a, b, c;
+    printf("A = ");
+    scanf("%lg", &a);
+    printf("B = ");
+    scanf("%lg", &b);
+    printf("C = ");
+    scanf("%lg", &c);
+    masodfoku(a, b, c);
+    return 0;
+}
+    valtozoBeolvasas();
+*/
+//sok szam atlaga
+int atlagolo() {
+    double input, osszeg;
+    int num;
+    osszeg = 0.0;
+    num = 0;
+    printf("Sok szam atlaga:\nElso szam:");
+    scanf("%lg", &input);
+    while (input != 0.0) {
+        osszeg += input;
+        num++;
+        printf("Kovetkezo szam? (utolso 0)");
+        scanf("%lg", &input);
+    }
+    printf("A(z) %d szam atlaga: %g\n", num, osszeg/num);
+    return 0;
+}
+//haromjegyu palindromszamok
+int palindromok() {
+    int a, b;
+    for (a = 0; a < 10; a++) {
+        for (b = 1; b < 10; b++) {
+            printf("%d%d%d, ", b, a, b);
+        }
+    }
+    return 0;
+}
+//ketdimenzios vektor elforgatasa
+int forgatas() {
+    double x, y, alpha, x_rotated, y_rotated;
+    printf("X = "); scanf("%lg", &x);
+    printf("Y = "); scanf("%lg", &y);
+    printf("Szog = "); scanf("%lg", &alpha);
+
+    alpha = alpha * M_PI / 180.0;
+
+    x_rotated = x * cos(alpha) - y * sin(alpha);
+    y_rotated = x * sin(alpha) + y * cos(alpha);
+
+    printf("Az elforgatott vektor V(%g, %g)\n", x_rotated, y_rotated);
+    return 0;
+}
+//szam szamjegyeinek osszege
+int szamjegyosszeg(int input) {
+    int sum, modulo;
+    sum = 0;
+    modulo = 0;
+    //printf("Adjon meg egy szamot!\nszam = "); scanf("%d", &input);
+    while (input != 0) {
+        modulo = input % 10;
+        sum += modulo;
+        input = (input - modulo) / 10;
+    }
+    //printf("A szamjegyek osszege: %d\n", sum);
+    return sum;
+}
+//harom szamrol eldonti, lehet-e egy haromszog harom oldala
+int haromszogoldal() {
+    double a, b, c;
+    printf("A = "); scanf("%lg", &a);
+    printf("B = "); scanf("%lg", &b);
+    printf("C = "); scanf("%lg", &c);
+    if (a < 0 || b < 0 || c < 0)
+        printf("Negativ szam nem lehet.\n");
+    else if (a + b > c && a + c > b && b + c > a)
+        printf("Ebbol lehet haromszog.\n");
+    else
+        printf("Nem lehet haromszog.\n");
+    return 0;
+}
+//legnagyobb haromjegyu szam, mely szamjegyeinek osszege megegyezik a primtenyezoinek osszegevel
+int primesosszeg() {
+    int input;
+    for(input = 999; input > 0; input--) {
+        if (szamjegyosszeg(input) == primtenyezokosszege(input)){
+            printf("A legnagyobb ilyen szam: %d\n", input);
+            return 0;
+        }
+    }
+    return 0;
+}
+//szam ket tizedesjegyet bepakolja egy valtozoba.
+int doubletoint(double input){
+    int e_input;
+    double tizedek;
+    if (input < 0) input *= (-1);
+    e_input = input / 1;
+    tizedek = input - e_input;
+    printf("tizedek: %g\n", tizedek*100);
+    e_input = tizedek * 1000 / 10;
+    printf("egeszresze: %d\n", e_input);
+
+    return e_input;
+}
+int dtoi(){
+    double input;
+    printf("Szam = "); scanf("%lg", &input);
+    doubletoint(input);
+    return 0;
+}
+//sok szam kozul a legnagyobb
+int biggest() {
+    double biggest, input;
+    //biggest = 0;
+    printf("Adjon meg egy szamot (0.0 vege): "); scanf("%lg", &input);
+    if (input != 0.0) {
+        biggest = input;
+    } else {
+        printf("Nincs legnagyobb:(\n");
+        return 0;
+    }
+    while (input != 0.0) {
+        if (input > biggest) biggest = input;
+        printf("Adjon meg egy szamot (0.0 vege): "); scanf("%lg", &input);
+    }
+    printf("A legnagyobb beirt szam %g volt.\n", biggest);
+    return 0;
+}
+//10-es szorzotabla
+int szorzotabla() {
+    int first, next;
+    for (first = 0; first < 10; first++) {
+        for (next = 0; next < 10; next++) {
+            if (first == 0 && next == 0) printf("\t");
+            if (next != 0 && first == 0) printf("%d\t", next);
+            if (next == 0 && first != 0) printf("%d\t", first);
+            if (next != 0 && first != 0) printf("%d\t", first*next);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//tokeletes szam-e
+int osztoosszeg(int input) {
+    int result, oszto;
+    result = 0;
+    for (oszto = 1; oszto <= input/2; oszto++) {
+        if (input % oszto == 0) result += oszto;
+    }
+    //printf("osszeg: %d", result);
+    //if (input == result) printf("tokeletes szam\n");
+    return result;
+}
+//eldonti, hogy baratsagos szamparok-e
+int baratsagosSzamparok() {
+    int a, b;
+    printf("Elso szam: "); scanf("%d", &a);
+    printf("Masik szam: "); scanf("%d", &b);
+    //tmp_a = osztoosszeg(a);
+    //tmp_b = osztoosszeg(b);
+    if (b == osztoosszeg(a) && a == osztoosszeg(b)) printf("A(z) %d es %d szampar baratsagos.\n", a, b);
+    else printf("Nem baratsagos szampar.\n");
+    return 0;
+}
+int main()
+{
+    baratsagosSzamparok();
+    printf("Hello world!\n");
+    return 0;
+}
